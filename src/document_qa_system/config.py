fbb3de073ca5029b4ffcc156906defc9bebed402
Vetrivel A <vetrivel.a2022ai-ds@sece.ac.in>
2026-01-08 20:56:10 +0530

    Configuration completed

---
+++ src/document_qa_system/config.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+from typing import Optional 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    #Base Path
+    BASE_DIR = Path(__file__).parent.parent.parent
+    DATA_DIR = BASE_DIR / "data"
+    UPLOAD_DIR = DATA_DIR / "uploads"
+    PROCESSED_DIR = DATA_DIR / "processed"
+
+    #API Keys
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY","")
+
+     # Model Configuration
+    GROQ_MODEL: str = os.getenv("GROQ_MODEL", "llama-3.3-70b-versatile")
+    EMBEDDING_MODEL: str = os.getenv(
+        "EMBEDDING_MODEL", 
+        "sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    # Vector Store
+    CHROMA_PERSIST_DIR: Path = Path(
+        os.getenv("CHROMA_PERSIST_DIR", "./data/chroma_db")
+    )
+
+    # Document Processing
+    CHUNK_SIZE: int = 1000
+    CHUNK_OVERLAP: int = 200
+
+    # Supported file formats
+    SUPPORTED_FORMATS = {'.pdf', '.txt', '.docx'}
+    MAX_FILE_SIZE_MB = 50
+
+    @classmethod
+    def validate(cls) -> None:
+        if not cls.GROQ_API_KEY:
+            raise ValueError(
+                "GROQ_API_KEY not found in environment variables. "
+                "Please set it in your .env file."
+            )
+    
+        cls.UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+        cls.PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+        cls.CHROMA_PERSIST_DIR.mkdir(parents=True, exist_ok=True)
+
+        print("validated successfully!")
+        print(f"Upload directory: {cls.UPLOAD_DIR}")
+        print(f"Using model: {cls.GROQ_MODEL}")
+        print(f"Using embeddings: {cls.EMBEDDING_MODEL}")
+
+Config.validate()
